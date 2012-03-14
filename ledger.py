988ac97d8909a1833e84f625e0de2594d5c5f88c
Kevin Daudt <compufreak@gmail.com>
2012-03-15 00:00:37 +0100

    Improve the help output

--- ledger.py
+++ ledger.py
@@ -4,10 +4,20 @@ from ledger import ledger
 import curses
 
 parser = argparse.ArgumentParser(description="Adds entries to a ledger file")
-parser.add_argument('action')
-parser.add_argument('input')
-parser.add_argument('--ledger-file', dest='ledger_file', nargs=1)
-parser.add_argument('--output', dest='output', nargs=1)
+parser.add_argument('action', help="Currently only 'parsecsv'")
+parser.add_argument('input', help="The file to read the statements from, currently only ABN AMRO tsv's are supported")
+parser.add_argument(
+    '--ledger-file',
+    dest='ledger_file',
+    nargs=1,
+    help="The ledger file to get accounts from"
+)
+parser.add_argument(
+    '--output',
+    dest='output',
+    nargs=1,
+    help="The file to write the output to. Use - for stdout"
+)
 
 args = parser.parse_args()
 
